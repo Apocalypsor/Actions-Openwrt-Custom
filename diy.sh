53f49a7817329d92ef44d74d49e25c9b5a02484c
Apocalypsor <Apocalypsor@users.noreply.github.com>
2019-11-25 01:46:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.4/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
 sed -i '\/bin\/ash -c "mentohust -u$username -p$password -n$ifname -b3 -w $cmd"/i\        sleep 60' openwrt/package/luci-app-mentohust/files/root/etc/init.d/mentohust
 sed -i '\/bin\/ash -c "mentohust -u$username -p$password -n$ifname -b3 -w $cmd"/a\        rm /tmp/mentohust.pid -f && /usr/sbin/mentohust -uU201610721 -p213014 -o119.29.29.29 -a1 -d1 -nmacvlan2 -b1' openwrt/package/luci-app-mentohust/files/root/etc/init.d/mentohust
