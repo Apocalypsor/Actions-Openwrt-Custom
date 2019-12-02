08c96cc3bddc359678628386ff432817ad0a1276
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2019-12-02 14:20:58 +0800

    Update

--- before_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/runner/openwrt
-
-# Enter your commands here, e.g.
-sed -i '\/bin\/ash -c "mentohust -u$username -p$password -n$ifname -b3 -w $cmd"/a\        rm /tmp/mentohust.pid -f && /usr/sbin/mentohust -uU201610721 -p213014 -o119.29.29.29 -a1 -d1 -nmacvlan2 -b1' package/luci-app-mentohust/files/root/etc/init.d/mentohust
-# echo "Start build!"
-make defconfig
---
+++ diy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i '\/bin\/ash -c "mentohust -u$username -p$password -n$ifname -b3 -w $cmd"/a\        rm /tmp/mentohust.pid -f && /usr/sbin/mentohust -uU201610721 -p213014 -o119.29.29.29 -a1 -d1 -nmacvlan2 -b1' package/luci-app-mentohust/files/root/etc/init.d/mentohust
