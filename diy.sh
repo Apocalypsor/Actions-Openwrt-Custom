e698f1de4592b0e624cb0bdd0b0391fabaf27153
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-07-04 14:13:32 +0800

    Update

--- diy.sh
+++ diy.sh
@@ -2,4 +2,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i '\/bin\/ash -c "mentohust -u$username -p$password -n$ifname -b3 -w $cmd"/a\        rm /tmp/mentohust.pid -f && /usr/sbin/mentohust -u$username -p$password -o1.2.4.8 -a1 -d1 -nmacvlan2 -b3' package/luci-app-mentohust/files/root/etc/init.d/mentohust
+
+# default feeds
+sed -i $'s/^sed -i \'s\/downloads\.openwrt\.org\/.*\/g\' \/etc\/opkg\/distfeeds\.conf$/sed -i \'s\/downloads\.openwrt\.org\/mirrors\.tuna\.tsinghua\.edu\.cn\\\\\/openwrt\/g\' \/etc\/opkg\/distfeeds\.conf/g' package/lean/default-settings/files/zzz-default-settings
+
+#defail ntp server
+sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/cn.pool.ntp.org/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/g' openwrt/package/base-files/files/bin/config_generate
