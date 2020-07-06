c1f5462bf9e4b86b269b486d3fe561d541e04d9e
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-07-06 17:05:08 +0800

    fix

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 sed -i $'s/^sed -i \'s\/downloads\.openwrt\.org\/.*\/g\' \/etc\/opkg\/distfeeds\.conf$/sed -i \'s\/downloads\.openwrt\.org\/mirrors\.tuna\.tsinghua\.edu\.cn\\\\\/openwrt\/g\' \/etc\/opkg\/distfeeds\.conf/g' package/lean/default-settings/files/zzz-default-settings
 
 #defail ntp server
-sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/1.openwrt.pool.ntp.org/cn.pool.ntp.org/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/3.openwrt.pool.ntp.org/ntp.sjtu.edu.cn/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/ntp.sjtu.edu.cn/g' package/base-files/files/bin/config_generate
