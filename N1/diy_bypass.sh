e125c4fb7ab07b5d798b77b8995826044f93a7b0
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-10-19 15:08:57 +0800

    Update

---
+++ N1/diy_bypass.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+# default feeds
+sed -i $'s/^sed -i \'s\/downloads\.openwrt\.org\/.*\/g\' \/etc\/opkg\/distfeeds\.conf$/sed -i \'s\/downloads\.openwrt\.org\/mirrors\.tuna\.tsinghua\.edu\.cn\\\\\/openwrt\/g\' \/etc\/opkg\/distfeeds\.conf/g' package/lean/default-settings/files/zzz-default-settings
+
+#defail ntp server
+sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/ntp.sjtu.edu.cn/g' package/base-files/files/bin/config_generate
